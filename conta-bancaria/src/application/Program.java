dd52ff05b2fedcefd8e5699b43c30a5829d1322c
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2026-01-02 11:30:29 -0300

    Feat: App Funcionando

---
+++ conta-bancaria/src/application/Program.java
@@ -0,0 +1,87 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Conta;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+
+		
+		int numeroConta = 0, opcao = 0;
+		String nomeTitular = "", deposito = "";
+		double saldo = 0.00;
+		
+		System.out.println("Banco do Yuri");
+		
+		System.out.print("Digite o número da conta: ");
+		numeroConta = scan.nextInt();
+		
+		System.out.print("Digite seu nome completo: ");
+		nomeTitular = scan.next();
+		
+		do {
+			
+			System.out.print("Haverá deposito inicial? (\"sim\", \"nao\"): ");
+			deposito = scan.next().toLowerCase();
+			
+			if(!deposito.equals("sim") && !deposito.equals("nao")) {
+				System.out.println("Por favor, digite uma opção válida");
+			}
+			
+		} while(!deposito.equals("sim") && !deposito.equals("nao"));
+		
+		if(deposito.equals("sim")) {
+			System.out.print("Digite o saldo inicial: R$");
+			saldo = scan.nextDouble();
+		}
+		
+		Conta conta = new Conta(numeroConta, nomeTitular, saldo);
+				
+		do {
+			System.out.println("");
+			System.out.println("Menu de opções: ");
+			System.out.println("1) Fazer depósito de valor; ");
+			System.out.println("2) Sacar valor; ");
+			System.out.println("0) Sair da aplicação. ");
+			System.out.println("");
+			System.out.print("Digite a opção desejada: ");
+			opcao = scan.nextInt();
+
+			System.out.println("");
+			
+			switch(opcao) {
+				case 1:
+					System.out.print("Digite o valor do depósito: R$");
+					saldo = scan.nextDouble();
+					
+					conta.deposito(saldo);
+					break;
+				case 2:
+					System.out.print("Digite o valor do saque: R$");
+					saldo = scan.nextDouble();
+					
+					conta.saque(saldo);
+					break;
+				case 0:
+					System.out.println("Aplicação encerrada!");
+					break;
+				default:
+					System.out.println("Digite uma opção válida!");
+			}
+
+			System.out.println("");
+
+			System.out.println(conta.toString());
+		} while(opcao != 0);
+		
+		scan.close();
+
+	}
+
+}
---
+++ conta-bancaria/src/entities/Conta.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class Conta {
+	private int numeroConta;
+	private String nomeTitular;
+	private double saldo;
+
+	public Conta(int numeroConta, String nomeTitular) {
+		this.numeroConta = numeroConta;
+		this.nomeTitular = nomeTitular;
+		saldo = 0.00;
+	}
+
+	public Conta(int numeroConta, String nomeTitular, double saldo) {
+		this.numeroConta = numeroConta;
+		this.nomeTitular = nomeTitular;
+		this.saldo = saldo;
+	}
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+	public String getNomeTitular() {
+		return nomeTitular;
+	}
+
+	public void setNomeTitular(String nomeTitular) {
+		this.nomeTitular = nomeTitular;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void deposito(double valor) {
+		saldo += valor;
+	}
+
+	public void saque(double valor) {
+		saldo -= (valor + 5);
+	}
+
+	public String toString() {
+		return "Conta n°" + numeroConta + "\nSr(a): " + nomeTitular + "\nSaldo disponível: R$" + String.format("%.2f", saldo);
+	}
+}
